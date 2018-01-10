1f59db3e738d47a93072c0069b17c0628e3e64ce
raqueloureiro <34480148+raqueloureiro@users.noreply.github.com>
2018-01-10 23:47:08 +0000

    Add files via upload

--- javascript/shop.js
+++ javascript/shop.js
@@ -1,23 +1,36 @@
-window.onload =function(){
-init();
+window.onload = function () {
+    init();
 
-function init(){
-    console.log ('init fired');
-	}
+    function init() {
+        console.log('init fired');
+    }
 }
 
-$(document).ready(function() {
-var itemCount = 0;
+function loadLocalCart() {
+    //we use json.parse to return the stringified object back to a complex object.
+    cartShop = JSON.parse(localStorage.getItem("cartSession"));
+    if (!cartShop) cartShop = [];
+};
+$(window).on("load", function () {
+    var itemCount;
 
-$('.add').click(function (){
-  itemCount ++;
-  $('#itemCount').html(itemCount).css('display', 'block');
-}); 
+    if (localStorage && localStorage.getItem("itemCount")) {
+        itemCount = localStorage.getItem("itemCount")
+        $('#itemCount').html(itemCount).css('display', 'block');
+    } else {
+        itemCount = 0;
+    }
 
-$('.clear').click(function() {
-  itemCount = 0;
-  $('#itemCount').html('').css('display', 'none');
-  $('#cartItems').html('');
-})
-}); 	
+    $('.add').click(function () {
+        itemCount++;
+        $('#itemCount').html(itemCount).css('display', 'block');
+        localStorage.setItem("itemCount", itemCount)
+    });
 
+    $('.clear').click(function () {
+        itemCount = 0;
+        $('#itemCount').html('').css('display', 'none');
+        $('#cartItems').html('');
+        localStorage.setItem("itemCount", 0)
+    })
+});
