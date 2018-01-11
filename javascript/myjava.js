3e64ebb52616eaff6e46b9d9a53715f2fd2bc1ea
raqueloureiro <34480148+raqueloureiro@users.noreply.github.com>
2018-01-11 12:35:53 +0000

    Add files via upload

--- javascript/myjava.js
+++ javascript/myjava.js
@@ -1,20 +1,20 @@
-window.onload =function(){
+window.onload =function(){				//I use this onload event to check the console and make sure my javascript has linked correctly
 init();
 
 function init(){
     console.log ('init fired');
-	}
+	}									//This types in the console that init was fired, confirming that everything is linked and the DOM is loaded and ready to recieve my next functions
 }
 
-$(document).ready(function() {
+$(document).ready(function() {			//The DOM is ready for JavaScript code to execute because I am including it in a ready function, JQuery knows its safe to run
 
-$("#slideshow > div:gt(0)").hide();
+$("#slideshow > div:gt(0)").hide();		//This hides my divs and allows the JS to scroll through them from fresh, there would be this strange untimely loop if not
 
-setInterval(function() { 
+setInterval(function() { 				//The INTERVAL is key here as it makes the execution of the function continuous allowing a smooth loop through the divs
   $('#slideshow > div:first')
-    .fadeOut(1000)
+    .fadeOut(800)
     .next()
-    .fadeIn(1000)
+    .fadeIn(800)						//This sets the timing for when the divs should scroll through and change the images on the slideshow
     .end()
     .appendTo('#slideshow');
 },  3000);
--- javascript/shop.js
+++ javascript/shop.js
@@ -1,35 +1,35 @@
-window.onload = function () {
+window.onload = function () {       //I use this onload event to check the console and make sure my javascript has linked correctly
     init();
 
     function init() {
         console.log('init fired');
-    }
+    }                               //This types in the console that init was fired, confirming that everything is linked and the DOM is loaded and ready to recieve my next functions
 }
 
-function loadLocalCart() {
-    //we use json.parse to return the stringified object back to a complex object.
-    cartShop = JSON.parse(localStorage.getItem("cartSession"));
+function loadLocalCart() {          //This fuction calls for my local cart
+//using json.parse returns the stringified object back to a complex object
+    cartShop = JSON.parse(localStorage.getItem("cartSession"));         //saves my data within local storage
     if (!cartShop) cartShop = [];
 };
-$(window).on("load", function () {
+$(window).on("load", function () {          //This is a jQuery event that is fired when DOM is loaded, allowing me to access local storage when teh docuemnt strucure is ready
     var itemCount;
 
-    if (localStorage && localStorage.getItem("itemCount")) {
+    if (localStorage && localStorage.getItem("itemCount")) {        //checks if there is anything stored
         itemCount = localStorage.getItem("itemCount")
         $('#itemCount').html(itemCount).css('display', 'block');
     } else {
-        itemCount = 0;
+        itemCount = 0;                                              //if nothing is stored and the user hasnt added anything to the cart then it remains empty
     }
 
     $('.add').click(function () {
-        itemCount++;
+        itemCount++;                                                //this adds on an item each time the user clicks to add to their cart and then stores this
         $('#itemCount').html(itemCount).css('display', 'block');
         localStorage.setItem("itemCount", itemCount)
     });
 
-    $('.clear').click(function () {
+    $('.clear').click(function () {                     //clears the storage and reverts the cart back to 0 items
         itemCount = 0;
-        $('#itemCount').html('').css('display', 'none');
+        $('#itemCount').html('').css('display', 'block');
         $('#cartItems').html('');
         localStorage.setItem("itemCount", 0)
     })
